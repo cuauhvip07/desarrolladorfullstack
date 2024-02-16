a1af2035316d858ce580a4a72f82b36f6f507b44
cuauhvip07 <cuauvillalba1@gmail.com>
2024-02-15 19:37:15 -0600

    Introduccion PHP
    Tema: Funciones

--- Extras/4IntroduccionPHP_inicio/15-funciones.php
+++ Extras/4IntroduccionPHP_inicio/15-funciones.php
@@ -1,6 +1,13 @@
 <?php include 'includes/header.php';
 
+// Puede pasrle int, float, array, bool
+function sumar(int $numero1 = 0,int $numero2 = 0){
+    echo $numero1 + $numero2;
+}
 
+sumar(10,20);
+echo "<br>";
+sumar(10);
 
 
 
