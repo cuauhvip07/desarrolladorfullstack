2185810403dbae49b61d3d9d2ea585094780c934
cuauhvip07 <cuauvillalba1@gmail.com>
2024-02-16 19:01:44 -0600

    Proyecto Bienes Raices
    Se inicia el proyecto de bienes con la pagina de inicio

--- 07_bienesraices_iniciocrud/gulpfile.js
+++ 07_bienesraices_iniciocrud/gulpfile.js
@@ -8,9 +8,7 @@ const concat = require('gulp-concat');
 const terser = require('gulp-terser-js');
 const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin'); // Minificar imagenes 
-const notify = require('gulp-notify');
 const cache = require('gulp-cache');
-const clean = require('gulp-clean');
 const webp = require('gulp-webp');
 
 const paths = {
@@ -23,7 +21,7 @@ function css() {
     return src(paths.scss)
         .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(postcss([autoprefixer()])) //,cssnano()
         // .pipe(postcss([autoprefixer()]))
         .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css'));
@@ -31,36 +29,34 @@ function css() {
 
 function javascript() {
     return src(paths.js)
-      .pipe(sourcemaps.init())
-      .pipe(concat('bundle.js'))
-      .pipe(terser())
-      .pipe(sourcemaps.write('.'))
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./build/js'))
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js'))
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest('./build/js'))
 }
 
 function imagenes() {
     return src(paths.imagenes)
         .pipe(cache(imagemin({ optimizationLevel: 3 })))
-        .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Imagen Completada' }));
+        .pipe(dest('build/img'));
 }
 
 function versionWebp() {
     return src(paths.imagenes)
         .pipe(webp())
         .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Imagen Completada' }));
 }
 
 
 function watchArchivos() {
     watch(paths.scss, css);
     watch(paths.js, javascript);
-    watch(paths.imagenes, imagenes);
-    watch(paths.imagenes, versionWebp);
+    // watch(paths.imagenes, imagenes);
+    // watch(paths.imagenes, versionWebp);
 }
 
 exports.css = css;
 exports.watchArchivos = watchArchivos;
-exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos); 
+exports.default = parallel(css, javascript, watchArchivos); 
