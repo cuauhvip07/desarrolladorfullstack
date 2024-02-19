8dbb09694c91bb46c97d4ac4ed477bc27b1d1b06
cuauhvip07 <cuauvillalba1@gmail.com>
2024-02-19 15:28:53 -0600

    Menú hamburguesa
    Se hace el menú de hamburguesa para dispositivos moviles

---
+++ 05_bienesraices_iniciocrud/src/js/app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    eventListeners()
+})
+
+function eventListeners(){
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.addEventListener('click', navegacionResponsive)
+}
+
+function navegacionResponsive(){
+    const navegacion = document.querySelector('.navegacion');
+    navegacion.classList.toggle('mostrar');
+}
