687e800b197292ea4d635ce9707687f64e720fa3
cuauhvip07 <cuauvillalba1@gmail.com>
2024-02-29 20:01:31 -0600

    Actualicacion header
    Se cambia las reedirecciones hacia las paginas web para que no marque error

--- 05_bienesraices_iniciocrud/includes/templates/header.php
+++ 05_bienesraices_iniciocrud/includes/templates/header.php
@@ -22,10 +22,10 @@
                 <div class="derecha">
                     <img class="dark-mode-boton" src="/build/img/dark-mode.svg" alt="dark-mode">
                     <nav class="navegacion">
-                        <a href="nosotros.php">Nosotros</a>
-                        <a href="anuncios.php">Anuncios</a>
-                        <a href="blog.php">Blog</a>
-                        <a href="contacto.php">Contacto</a>
+                        <a href="../../nosotros.php">Nosotros</a>
+                        <a href="../../anuncios.php">Anuncios</a>
+                        <a href="../../blog.php">Blog</a>
+                        <a href="../../contacto.php">Contacto</a>
                     </nav>
                 </div>
 
