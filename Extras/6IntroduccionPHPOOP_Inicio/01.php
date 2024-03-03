a0e4ace5750597283a72a86547df3f9a6b220302
cuauhvip07 <cuauvillalba1@gmail.com>
2024-03-02 21:37:26 -0600

    Atributos y constructores
    Uso de los atributos y constructores

--- Extras/6IntroduccionPHPOOP_Inicio/01.php
+++ Extras/6IntroduccionPHPOOP_Inicio/01.php
@@ -1,4 +1,6 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types = 1);
+include 'includes/header.php';
 
 // Definir una clase
 class Producto{
@@ -6,18 +8,23 @@ class Producto{
     public $precio;
     public $disponible;
 
-
+    public function __construct(string $nombre,int $precio, bool $diponible){
+        $this -> nombre = $nombre;
+        $this -> precio = $precio;
+        $this -> disponible = $diponible;
+    }
 }
 
-$producto = new Producto();
-$producto -> nombre = 'Comida';
-$producto -> precio = 200;
-$producto -> disponible = true;
+$producto = new Producto('Tablet',200,true);
+// $producto -> nombre = 'Comida';
+// $producto -> precio = 200;    Manera para poder llenar info
+// $producto -> disponible = true;
 
 echo "<pre>";
 var_dump($producto -> nombre);
 var_dump($producto -> precio);
 var_dump($producto -> disponible);
+var_dump($producto);
 echo "</pre>";
 
 
