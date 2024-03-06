961b65d69ef79776633caa9f31d8e965a243ca07
cuauhvip07 <cuauvillalba1@gmail.com>
2024-03-05 19:04:45 -0600

    Namespaces
    Como mandar a llamar clases desde otras paginas

--- Extras/6IntroduccionPHPOOP_Inicio/08.php
+++ Extras/6IntroduccionPHPOOP_Inicio/08.php
@@ -1,4 +1,28 @@
 <?php include 'includes/header.php';
 
+use App\Clientes;
+use App\Detalles;
+
+// // Incluir las otras clases
+// require 'clases/Clientes.php';
+// require 'clases/Detalles.php';
+
+function mi_autoload($clase){
+    $partes = explode('\\',$clase);
+    require  __DIR__.'/clases/'.$partes[1].'.php';
+}
+// Traer las clases que vayamos requiriendo
+spl_autoload_register('mi_autoload');
+
+// class Clientes{
+//     public function __construct()
+//     {
+//         echo "Desde 08.php y la clase cliente";
+//     }
+// }
+
+$detalles = new Detalles();
+$clientes = new Clientes();
+$clientes2 = new Clientes();
 
 include 'includes/footer.php';
---
+++ Extras/6IntroduccionPHPOOP_Inicio/clases/Clientes.php
@@ -0,0 +1,10 @@
+<?php 
+// namespace poner sobrenombres y no choque con otras clases
+namespace App;
+
+class Clientes {
+    public function __construct()
+    {
+        echo "Desde la clase de clientes";
+    }
+}
