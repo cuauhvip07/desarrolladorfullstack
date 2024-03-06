c0cf54d948d3dda40c005cca1192d852b2e3187b
cuauhvip07 <cuauvillalba1@gmail.com>
2024-03-05 19:45:13 -0600

    PDO
    Usar la bd con PDO

--- Extras/6IntroduccionPHPOOP_Inicio/10.php
+++ Extras/6IntroduccionPHPOOP_Inicio/10.php
@@ -1,4 +1,21 @@
 <?php include 'includes/header.php';
 
+// Conectar a la bd con PDO
+$db = new PDO('mysql:host=localhost; dbname=bienesraices_crud','root','');
+$query = "SELECT titulo,imagen FROM propiedades;";
 
+// //Consular la bd -> Sin sentencias preparadas
+// $propiedades = $db -> query($query) -> fetch();
+// var_dump($propiedades);
+
+// Sentencias preparadas
+$stmt = $db->prepare($query);
+$stmt->execute();
+$resultado = $stmt->fetchAll( PDO::FETCH_ASSOC);
+
+foreach($resultado as $propiedad):
+    echo $propiedad['titulo'];
+    echo "<br>";
+    echo $propiedad['imagen'];
+endforeach;
 include 'includes/footer.php';
