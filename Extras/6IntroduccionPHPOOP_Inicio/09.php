21ab20801498e6d2fe2e4c5fa8238cddb0fbf78d
cuauhvip07 <cuauvillalba1@gmail.com>
2024-03-05 19:35:04 -0600

    Consultas bd con POO y Mysql
    Nuevos métodos para la consulta y como evitar ser hackeados y ganar perfomance

--- Extras/6IntroduccionPHPOOP_Inicio/09.php
+++ Extras/6IntroduccionPHPOOP_Inicio/09.php
@@ -1,4 +1,29 @@
 <?php include 'includes/header.php';
 
+// Conectar a la bd
+$db = new mysqli('localhost','root','','bienesraices_crud');
 
+// Creacion del query
+$query = "SELECT titulo,imagen FROM propiedades;";
+
+// Lo preparamos
+$stmt = $db -> prepare($query);
+
+// Ejecutamos
+$stmt -> execute();
+
+// Le pones la variable que quieras
+$stmt -> bind_result($titulo,$imagen);
+
+// Asignamos el resultado 
+// $stmt->fetch();
+
+// Imprimomos el resultado
+// var_dump($titulo);
+// var_dump($imagen);
+
+// Imprimir todos los resultados
+while($stmt -> fetch()):
+    var_dump($titulo,$imagen);
+endwhile;
 include 'includes/footer.php';
